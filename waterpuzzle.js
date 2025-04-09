b075e51ef4b6a31e290895cf1ee9946130a1bbc3
5b1g0030 <5b1g0030@stust.edu.tw>
2025-04-09 11:55:26 +0800

    新增註解

--- waterpuzzle.js
+++ waterpuzzle.js
@@ -1,8 +1,9 @@
 // ()=>箭頭函數，作為事件處理器，()裡可傳入參數
+// document.getElementById("id"):從 HTML 文件中尋找並獲取特定 ID 的元素
 document.addEventListener("DOMContentLoaded",()=>{ 
-    const gameContainer = document.getElementById("game-container");
-    const playButton = document.getElementById("play-button");
-    const levelSelect = document.getElementById("level-select"); // 從 HTML 文件中尋找並獲取特定 ID 的元素
+    const gameContainer = document.getElementById("game-container"); // 遊戲區(div)
+    const playButton = document.getElementById("play-button"); // 遊戲開始按鈕
+    const levelSelect = document.getElementById("level-select"); // 關卡選項(下拉選單)
 
     const tubes = []; // 試管
     let levelCount = 1; // 關卡編號(預設為 1)
@@ -12,6 +13,7 @@ document.addEventListener("DOMContentLoaded",()=>{
         levelCount = level;
         document.getElementById("level-count").textContent = levelCount;
     }
+    
     // ----- 選擇關卡(下拉選單)
     // 事件監聽器："change" 表示當下拉選單的值改變時觸發
     levelSelect.addEventListener("change",(event)=>{
@@ -21,6 +23,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     });
 
     // ----- 開始遊戲(按鈕)
+    // 事件監聽器："click" 表示被點擊時觸發
     playButton.addEventListener("click",()=>{
         alert("遊戲開始"); // 彈出"遊戲開始"的訊息
         
